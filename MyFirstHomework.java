440996db71d3fa344146d01fa09c857d2e19fe62
minaxivaland <minaxi_valand@hotmail.comgit config --global user.email minaxi_valand@hotmail.com>
2022-04-02 12:02:19 +0100

    this is my first java programme

---
+++ MyFirstHomework.java
@@ -0,0 +1,31 @@
+package com.learning.java;
+
+public class MyFirstHomework {
+
+    public static void main(String[] args){
+        System.out.println("Homework");
+        System.out.println("Software Testing");
+        System.out.println("What is softare");
+        System.out.println("*System Software *Application Software");
+        System.out.print("Qualification Of Tester");
+        System.out.println("*Open Listener *Keen Observer *Creative Mind *Optimistic *Keen Observer");
+        System.out.println("SDLC (Software Development Life Cycle");
+        System.out.println("STLC (Software Testing Life Cycle");
+        System.out.println("#Test Scenario #Test Case #Test Plan");
+        System.out.println("RTM (Requirement Traceability Matrix)");
+        System.out.println("ECP ( Equivalence Class Partitoning)");
+        System.out.println("BVA (Boundary Value Analysis)");
+        System.out.println("Severity 1.Critical 2.High 3.Medium 4.Low");
+        System.out.println("Priority *P1 *P2 *3");
+        System.out.println(" #Black Box Testing #White Box Testing ");
+        System.out.print(" Testing Levels ");
+        System.out.println(" 1Unite Testing 2Integration Testing 3System Testing 4Acceptance Testing");
+
+
+
+
+
+
+
+    }
+}
---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,8 @@
+package com.learning.java;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Minaxi Valand");
+    }
+}
